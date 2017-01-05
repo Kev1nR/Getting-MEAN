bef6af05eb163439b32e066b80057d38bad5d4de
Kev1nR <kevinroche@blueyonder.co.uk>
2017-01-05 09:34:48 +0000

    Revert change to other.js

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,10 +1,6 @@
 /* GET home page */
 module.exports.about = function(req, res, next) {
   res.render('generic-text', {
-<<<<<<< HEAD
-    title: 'About IT - Test',
-=======
-    title: 'About IT - ch05',
->>>>>>> 0cca13dc9d93146e45b4abcdeeba0d745ece37ed
+    title: 'About IT',
     content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscing tristique. Proin posuere semper tellus, id placerat augue dapibus ornare. Aenean leo metus, tempus in nisl eget, accumsan interdum dui. Pellentesque sollicitudin volutpat ullamcorper.' });
   };
