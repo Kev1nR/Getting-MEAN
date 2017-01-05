83b9ae4cd3fd013c6e28f202394c20034fc4f707
Kev1nR <kevinroche@blueyonder.co.uk>
2017-01-05 13:13:24 +0000

    Adding more mongoose elements

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,13 +1,45 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
+var theEarth = (function(){
+    var radius = 6371;
+    var getDistanceFromRadians = function (rads) {
+        return parseFloat(rads * radius);
+    };
+    var getRadsFromDistance = function(dist){
+        return parseFloat(dist / radius);
+    };
+
+    return {
+        getDistanceFromRadians : getDistanceFromRadians,
+        getRadsFromDistance : getRadsFromDistance
+    };
+})();
+
 var sendJsonResponse = function(res, status, content) {
     res.status(status);
     res.json(content);
 };
 
-module.exports.locationsListByDistance = function (req, res) { sendJsonResponse(res, 200, {"status" : "success"}); };
+
+
+module.exports.locationsListByDistance = function (req, res) { 
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+    var point = {
+        type: "Point",
+        coordinates: [lng, lat]
+        };
+    var geoOptions = { 
+        spherical: true,
+        num: 10 
+        };
+
+    Loc.geoNear(point, options, callback); 
+};
+
 module.exports.locationsCreate = function (req, res) { sendJsonResponse(res, 200, {"status" : "success"}); };
+
 module.exports.locationsReadOne = function(req, res) {
     if (req.params && req.params.locationid) {
         Loc.findById(req.params.locationid)
@@ -25,5 +57,6 @@ module.exports.locationsReadOne = function(req, res) {
                 sendJsonResponse(res, 404, {"message": "No locationid in request"});
     }
 };
+
 module.exports.locationsUpdateOne = function (req, res) { sendJsonResponse(res, 200, {"status" : "success"}); };
 module.exports.locationsDeleteOne = function (req, res) { sendJsonResponse(res, 200, {"status" : "success"}); };
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,9 +1,49 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+
 var sendJsonResponse = function(res, status, content) {
     res.status(status);
     res.json(content);
 };
 
 module.exports.reviewsCreate = function (req, res) { sendJsonResponse(res, 200, {"status" : "success"}); };
-module.exports.reviewsReadOne = function (req, res) { sendJsonResponse(res, 200, {"status" : "success"}); };
+
+module.exports.reviewsReadOne = function(req, res) {
+    if (req.params && req.params.locationid && req.params.reviewid) {
+        Loc.findById(req.params.locationid)
+           .select('name reviews')
+           .exec(
+                function(err, location) {
+                    var response, review;
+                    if (!location) { 
+                        sendJsonResponse(res, 404, {"message": "locationid not found"});
+                        return;
+                    } else if (err) {
+                        sendJsonResponse(res, 400, err);
+                        return;
+                    }
+                        if (location.reviews && location.reviews.length > 0) {
+                            var review = location.reviews.id(req.params.reviewid.toString());
+                            if (!review) {
+                                var resptext = "reviewid [" + req.params.reviewid + "] not found"
+                                sendJsonResponse(res, 404, {"message": resptext});
+                            } else {
+                                response = { location : {
+                                                name : location.name,
+                                                id : req.params.locationid
+                                                },
+                                             review : review
+                                           };
+                                sendJsonResponse(res, 200, response);
+                            }
+                    } else {
+                        sendJsonResponse(res, 404, {"message": "No reviews found"});
+                    }
+            });
+    } else {
+        sendJsonResponse(res, 404, {"message": "Not found, locationid and reviewid are both required"});
+    }
+};
+
 module.exports.reviewsUpdateOne = function (req, res) { sendJsonResponse(res, 200, {"status" : "success"}); };
 module.exports.reviewsDeleteOne = function (req, res) { sendJsonResponse(res, 200, {"status" : "success"}); };
