b2184365e8fb4abc9d526dc4e7db1450c1875cda
Kev1nR <kevinroche@blueyonder.co.uk>
2017-01-06 17:17:23 +0000

    adding non-working geoNear changes

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -21,21 +21,43 @@ var sendJsonResponse = function(res, status, content) {
     res.json(content);
 };
 
-
-
-module.exports.locationsListByDistance = function (req, res) { 
+module.exports.locationsListByDistance = function(req, res) {
     var lng = parseFloat(req.query.lng);
     var lat = parseFloat(req.query.lat);
     var point = {
         type: "Point",
         coordinates: [lng, lat]
-        };
-    var geoOptions = { 
+    };
+    var geoOptions = {
         spherical: true,
-        num: 10 
-        };
+        maxDistance: theEarth.getRadsFromDistance(35),
+        num: 10
+    };
 
-    Loc.geoNear(point, options, callback); 
+    if (!lng || !lat) {
+        sendJsonResponse(res, 404, {"message": "lng and lat query parameters are required"});
+        return;
+    }
+
+    Loc.geoNear(point, geoOptions, function(err, results, stats) {
+        var locations = [];
+        
+        if (err) {
+            sendJsonResponse(res, 404, err);
+        } else {
+            results.forEach(function(doc) {
+                locations.push({
+                    distance: theEarth.getDistanceFromRadians(doc.dis),
+                    name: doc.obj.name,
+                    address: doc.obj.address,
+                    rating: doc.obj.rating,
+                    facilities: doc.obj.facilities,
+                    _id: doc.obj._id
+                });
+            });
+            sendJsonResponse(res, 200, locations);
+        }
+    });
 };
 
 module.exports.locationsCreate = function (req, res) { sendJsonResponse(res, 200, {"status" : "success"}); };
